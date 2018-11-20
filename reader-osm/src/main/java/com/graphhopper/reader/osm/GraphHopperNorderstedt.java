0673d6ab9cd27daa217941e2d25b3d61e12a77d7
Peter <graphhopper@gmx.de>
2018-11-20 14:38:46 +0100

    minor customization for Norderstedt

---
+++ reader-osm/src/main/java/com/graphhopper/reader/osm/GraphHopperNorderstedt.java
@@ -0,0 +1,17 @@
+package com.graphhopper.reader.osm;
+
+import com.graphhopper.routing.util.EncodingManager;
+
+public class GraphHopperNorderstedt extends GraphHopperOSM {
+
+    public GraphHopperNorderstedt() {
+        EncodingManager encodingManager = new EncodingManager();
+        encodingManager.
+        setEncodingManager(encodingManager);
+    }
+    @Override
+    public void postProcessing() {
+        // DO CSV import
+        super.postProcessing();
+    }
+}
--- web-bundle/src/main/java/com/graphhopper/http/GraphHopperManaged.java
+++ web-bundle/src/main/java/com/graphhopper/http/GraphHopperManaged.java
@@ -21,8 +21,10 @@ package com.graphhopper.http;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.json.geo.JsonFeatureCollection;
+import com.graphhopper.reader.osm.GraphHopperNorderstedt;
 import com.graphhopper.reader.osm.GraphHopperOSM;
 import com.graphhopper.routing.lm.LandmarkStorage;
+import com.graphhopper.routing.util.EncodingManager;
 import com.graphhopper.routing.util.spatialrules.SpatialRuleLookupHelper;
 import com.graphhopper.util.CmdArgs;
 import com.graphhopper.util.Parameters;
@@ -44,25 +46,7 @@ public class GraphHopperManaged implements Managed {
     private final GraphHopper graphHopper;
 
     public GraphHopperManaged(CmdArgs configuration, ObjectMapper objectMapper) {
-        String splitAreaLocation = configuration.get(Parameters.Landmark.PREPARE + "split_area_location", "");
-        JsonFeatureCollection landmarkSplittingFeatureCollection;
-        try (Reader reader = splitAreaLocation.isEmpty() ? new InputStreamReader(LandmarkStorage.class.getResource("map.geo.json").openStream(), UTF_CS) : new InputStreamReader(new FileInputStream(splitAreaLocation), UTF_CS)) {
-            landmarkSplittingFeatureCollection = objectMapper.readValue(reader, JsonFeatureCollection.class);
-        } catch (IOException e1) {
-            logger.error("Problem while reading border map GeoJSON. Skipping this.", e1);
-            landmarkSplittingFeatureCollection = null;
-        }
-        graphHopper = new GraphHopperOSM(landmarkSplittingFeatureCollection).forServer();
-        String spatialRuleLocation = configuration.get("spatial_rules.location", "");
-        if (!spatialRuleLocation.isEmpty()) {
-            final BBox maxBounds = BBox.parseBBoxString(configuration.get("spatial_rules.max_bbox", "-180, 180, -90, 90"));
-            try (final InputStreamReader reader = new InputStreamReader(new FileInputStream(spatialRuleLocation), UTF_CS)) {
-                JsonFeatureCollection jsonFeatureCollection = objectMapper.readValue(reader, JsonFeatureCollection.class);
-                SpatialRuleLookupHelper.buildAndInjectSpatialRuleIntoGH(graphHopper, maxBounds, jsonFeatureCollection);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        graphHopper = new GraphHopperNorderstedt();
         graphHopper.init(configuration);
     }
 
